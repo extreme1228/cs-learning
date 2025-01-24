749221b8f798d6ebc2e3a4a12131e446cb2effc8
吕博文 <extreme1228@lvbowendeMacBook-Air.local>
2025-01-24 20:21:39 +0800

    commit

---
+++ cs61a/lab/lab01/lab01.py
@@ -0,0 +1,100 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    ans = 1
+    while k > 0:
+        ans *= n
+        n -= 1
+        k -= 1
+    return ans
+
+
+def divisible_by_k(n, k):
+    """
+    >>> a = divisible_by_k(10, 2)  # 2, 4, 6, 8, and 10 are divisible by 2
+    2
+    4
+    6
+    8
+    10
+    >>> a
+    5
+    >>> b = divisible_by_k(3, 1)  # 1, 2, and 3 are divisible by 1
+    1
+    2
+    3
+    >>> b
+    3
+    >>> c = divisible_by_k(6, 7)  # There are no integers up to 6 divisible by 7
+    >>> c
+    0
+    """
+    "*** YOUR CODE HERE ***"
+    res = []
+    val = k;
+    while val <= n:
+        res.append(val)
+        val += k
+    if(len(res) == 0):
+        print(0)
+    else:
+        for x in res:
+            print(x)
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    ans = 0
+    while y != 0:
+        ans += y % 10
+        y /= 10
+    return ans
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    ok = False
+    n_str = str(n)
+    for pos in range(len(n_str)):
+        if(pos == n_str - 1):
+            break
+        if(n_str[pos] == '8' and n_str[pos+1] == '8'):
+            ok = True
+            break;
+    print(ok)
